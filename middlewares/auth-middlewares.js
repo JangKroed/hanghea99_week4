9f96f5beaaefa662b1353e19e61480e5e5797cbf
JangKroed <didlsdydgh@gmail.com>
2022-10-10 01:41:53 +0900

    try in the 조건

--- middlewares/auth-middlewares.js
+++ middlewares/auth-middlewares.js
@@ -2,17 +2,16 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  const [tokenType, tokenValue] = authorization.split(" ");
-
-  if (tokenType !== "Bearer") {
-    res.status(401).send({
-      errorMessage: "로그인이 필요한 기능입니다.",
-    });
-    return;
-  }
-
   try {
+    const { authorization } = req.headers;
+    const [tokenType, tokenValue] = authorization.split(" ");
+
+    if (tokenType !== "Bearer") {
+      res.status(401).send({
+        errorMessage: "로그인이 필요한 기능입니다.",
+      });
+      return;
+    }
     const { userId } = jwt.verify(tokenValue, "MySecretKey");
     console.log("auth", userId);
     User.findOne({ userId: userId })
