90c887b391acd5c2f772382af57b4e636f7ebf14
JangKroed <didlsdydgh@gmail.com>
2022-10-10 18:48:58 +0900

    postId type String -> Number 변경

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
-  postId: String,
+  postId: Number,
   commentId: Number,
   userId: Number,
   nickname: String,
